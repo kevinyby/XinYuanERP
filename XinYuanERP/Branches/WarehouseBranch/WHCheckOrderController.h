3827daaaf0470e6c24ec1f0a4a444a18598c7df2
kevinyby <405437367@qq.com>
2014-07-18 16:56:41 +0800

    working

---
+++ XinYuanERP/Branches/WarehouseBranch/WHCheckOrderController.h
@@ -0,0 +1,13 @@
+//
+//  WHCheckOrderController.h
+//  XinYuanERP
+//
+//  Created by Xinyuan4 on 14-7-18.
+//  Copyright (c) 2014年 Xinyuan4. All rights reserved.
+//
+
+#import "JsonController.h"
+
+@interface WHCheckOrderController : JsonController
+
+@end
