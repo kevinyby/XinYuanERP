cdb71e6fef6020412b71fbf31f3f39a324497d91
isaacselement <isaacselement@gmail.com>
2014-07-21 18:01:06 +0800

    fixed small bug

--- XinYuanERP/Classes/Helper/ViewHelper/AppTableContentsHelper.h
+++
@@ -1,7 +0,0 @@
-
-@interface AppPageViewHelper : NSObject
-
-
-+(NSMutableArray*) insertValues: (NSMutableDictionary*)realContentsDictionary partRealContentDictionary:(NSDictionary*)partRealContentDictionary isTop:(BOOL)isTop;
-
-@end
